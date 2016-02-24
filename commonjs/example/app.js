089aba2b26804c2d58de7c639c9be395b1990b44
Angel Petkov <apetkov@appcelerator.com>
2016-02-23 16:07:10 -0800

    [MOD-2138] iOS&Android:  Example app crashes with 4.1.0.GA due to being unable to find files

--- commonjs/example/app.js
+++ commonjs/example/app.js
@@ -11,70 +11,66 @@
 var Cloud = require('ti.cloud');
 var WindowManager = require('helper/WindowManager');
 var Utils = require('helper/Utils');
-var Cloud = require('ti.cloud');
 Cloud.debug = true;
 
 // Include the window hierarchy.
 WindowManager.include(
-    '/windows/chats/table',
-    '/windows/checkins/table',
-    '/windows/clients/table',
-    '/windows/customObjects/table',
-    '/windows/emails/table',
-    '/windows/events/table',
-    '/windows/files/table',
-    '/windows/friends/table',
-    '/windows/geoFences/table',
-    '/windows/photoCollections/table',
-    '/windows/photos/table',
-    '/windows/places/table',
-    '/windows/posts/table',
-    '/windows/keyValues/table',
-    '/windows/likes/table',
-    '/windows/messages/table',
-    '/windows/pushNotifications/table',
-    '/windows/pushSchedules/table',
-    '/windows/reviews/table',
-    '/windows/social/table',
-    '/windows/status/table',
-    '/windows/users/table',
-    '/windows/accessControlLists/table',
-    '/windows/genericSendRequest'
+	'/views/chats/table',
+	'/views/checkins/table',
+	'/views/clients/table',
+	'/views/customObjects/table',
+	'/views/emails/table',
+	'/views/events/table',
+	'/views/files/table',
+	'/views/friends/table',
+	'/views/geoFences/table',
+	'/views/photoCollections/table',
+	'/views/photos/table',
+	'/views/places/table',
+	'/views/posts/table',
+	'/views/keyValues/table',
+	'/views/likes/table',
+	'/views/messages/table',
+	'/views/pushNotifications/table',
+	'/views/pushSchedules/table',
+	'/views/reviews/table',
+	'/views/social/table',
+	'/views/status/table',
+	'/views/users/table',
+	'/views/accessControlLists/table',
+	'/views/genericSendRequest'
 );
 
 // Define our main window.
 var table = Ti.UI.createTableView({
-    backgroundColor: '#fff',
-    data: Utils.createRows([
-        'Users',
-        'Access Control Lists',
-        'Chats',
-        'Checkins',
-        'Clients',
-        'Custom Objects',
-        'Emails',
-        'Events',
-        'Files',
-        'Friends',
-        'GeoFences',
-        'Key Values',
-        'Likes',
-        'Messages',
-        'Photo Collections',
-        'Photos',
-        'Places',
-        'Posts',
-        'Push Notifications',
-        'Push Schedules',
-        'Reviews',
-        'Social',
-        'Status',
-        'Generic Send Request'
-    ])
+	backgroundColor: '#fff',
+	data: Utils.createRows([
+		'Users',
+		'Access Control Lists',
+		'Chats',
+		'Checkins',
+		'Clients',
+		'Custom Objects',
+		'Emails',
+		'Events',
+		'Files',
+		'Friends',
+		'GeoFences',
+		'Key Values',
+		'Likes',
+		'Messages',
+		'Photo Collections',
+		'Photos',
+		'Places',
+		'Posts',
+		'Push Notifications',
+		'Push Schedules',
+		'Reviews',
+		'Social',
+		'Status',
+		'Generic Send Request'
+	])
 });
 table.addEventListener('click', WindowManager.handleOpenWindow);
 var win = WindowManager.createInitialWindow('ti.cloud demo', table);
 win.open();
-
-
-
